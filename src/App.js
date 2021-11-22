4ac8a2fb2dcc59994b0679cb80b4c11714cc3ebd
tanmay-choudhary <tanmaychoudhary4102@gmail.com>
2021-11-23 00:51:19 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(1);
+  const [data, setData] = useState("");
+  const [answer, setAns] = useState([]);
+  const handleChange = () => {
+    setCount(function (prevcount) {
+      return prevcount + 1;
+    });
+    setData("");
+    let currentAns = {
+      id: count,
+      data: data,
+    };
+    setAns([...answer, currentAns]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {count <= 3 ? (
+        <div className="flex">
+          <div className="ml-5">
+            <p>Question no. {count}?</p>
+          </div>
+          <div>
+            <input
+              value={data}
+              type="text"
+              name="data"
+              onChange={(event) => {
+                setData(event.target.value);
+              }}
+              align="left"
+            />
+            <button
+              className="bg-green-500 rounded h-10 w-16 "
+              onClick={handleChange}
+            >
+              next
+            </button>
+          </div>
+        </div>
+      ) : (
+        <ul>
+          {answer.map((ans) => {
+            return <li key={ans.id}>{ans.data}</li>;
+          })}
+        </ul>
+      )}
     </div>
   );
 }
